df8fa0a958818e5c1c47851a81e5667ce3ea713c
tanayghosh <tanay@azureiken.com>
2015-08-14 12:35:07 +0530

    Admin add products complete without service integration

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -1,10 +1,31 @@
  angular.module('luxire')
- .controller('adminController',function($scope){
+ .controller('adminController',function($scope, products, fileReader){
    $scope.navbar = "default";
    $scope.adminConsole = "default";
    $scope.isActive = false;
+
    $scope.activeButton = function(element) {
     $scope.isActive = !$scope.isActive;
     $scope.navbar = element;
   }
+
+	products.getProducts().then(function(data) {
+		console.log('admin');
+    console.log(data);
+		$scope.jsonresponse = data;
+    console.log($scope.jsonresponse);
+	}, function(info){
+		console.log(info);
+	})
+
+  //controller function for file reader
+$scope.uploadSponsorLogo = function(files) {
+        if (files && files.length) {
+        $scope.sponsorLogoFileName = files[0].name
+        fileReader.readAsDataUrl(files[0], $scope).then(function(result) {
+        $scope.productImage = result
+      })
+    }
+  }
+
 })
--- client/app/app.js
+++ client/app/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('luxire', ['ui.router','ngRoute','ui.bootstrap'])
+angular.module('luxire', ['ui.router','ngRoute','ui.bootstrap','angularFileUpload','ui.bootstrap.datetimepicker'])
 
 .run(function($location){
 	$location.path('/productdetails');
