95de6695cec5d58233fd98d15cd1cc857a52412b
Mudassir <mudassir@azureiken.com>
2016-03-04 20:19:53 +0530

    New cart Implementation

--- client/app/customer/cart/cart.controller.js
+++ client/app/customer/cart/cart.controller.js
@@ -1,5 +1,5 @@
 angular.module('luxire')
-.controller('CustomerCartController',function($scope, $state, ImageHandler, $rootScope, $stateParams, orders){
+.controller('CustomerCartController',function($scope, $state, ImageHandler, $rootScope, $stateParams, CustomerOrders, orders){
   console.log('CartController params', $stateParams);
   $scope.getImage = function(url){
     return ImageHandler.url(url);
@@ -9,16 +9,19 @@ angular.module('luxire')
   $scope.update_cart = function(line_items){
       console.log(line_items);
       angular.forEach(line_items, function(val, key){
-        orders.update_cart_by_quantity($rootScope.luxire_cart.number, $rootScope.luxire_cart.token, val.id, val.variant_id, val.quantity)
+        CustomerOrders.update_cart_by_quantity($rootScope.luxire_cart, val.id, val.variant_id, val.quantity)
         .then(function(data){
           console.log(data.data);
-          $rootScope.alerts.push({type: 'success', message: 'Cart updated successfully'});
-
         }, function(error){
-          $rootScope.alerts.push({type: 'danger', message: 'Failed to update cart'});
-
           console.error(error);
         })
+  });
+  CustomerOrders.get_order_by_id($rootScope.luxire_cart).then(function(data){
+    $rootScope.luxire_cart = data.data;
+    $rootScope.alerts.push({type: 'success', message: 'Cart updated successfully'});
+  }, function(error){
+    $rootScope.alerts.push({type: 'danger', message: 'Failed to update cart'});
+    console.error(error);
   });
 
 
@@ -36,48 +39,15 @@ angular.module('luxire')
   };
 
   $scope.checkout = function(){
-    $state.go('customer.checkout_address');
-  };
-
-  var proceed_to_checkout = function(order_number){
-    console.log(order_number);
-    orders.proceed_to_checkout(order_number, $rootScope.cart[0].checkoutObject.token).then(function(data){
-      $state.go('checkout_address',{checkoutObject: $rootScope.cart[0].checkoutObject});
+    console.log('proceed_to_checkout');
+    console.log($rootScope.luxire_cart);
+    CustomerOrders.proceed_to_checkout($rootScope.luxire_cart)
+    .then(function(data){
+      $state.go('customer.checkout_address');
       console.log(data);
-    },function(error){
-      $state.go('checkout_address',{checkoutObject: $rootScope.cart[0].checkoutObject});
+    }, function(error){
       console.error(error);
     })
-  }
-  $scope.go_to_checkout_address = function(quantity){
-    console.log('Quantity in response', $rootScope.cart[0].checkoutObject.line_items[0].quantity);
-    console.log('quantity in params', quantity);
-    order_number = $rootScope.cart[0].checkoutObject.number;
-    order_token = $rootScope.cart[0].checkoutObject.token;
-    line_item_id = $rootScope.cart[0].checkoutObject.line_items[0].id;
-    variant_id = $rootScope.cart[0].checkoutObject.line_items[0].variant_id;
+  };
 
-    if($rootScope.cart[0].checkoutObject.line_items[0].quantity != quantity){
-      orders.update_cart_by_quantity(order_number, order_token, line_item_id,variant_id,quantity).then(function(data){
-        orders.get_order_by_id(order_number, $rootScope.cart[0].checkoutObject.token).then(function(data){
-          console.log('updated order',data.data);
-          $rootScope.cart[0].checkoutObject = data.data;
-          proceed_to_checkout(order_number);
-        }, function(error){
-          console.error(error);
-        });
-          // $rootScope.cart[0].checkoutObject.line_items[0] = data.data;
-          console.log(data);
-          // proceed_to_checkout(order_number);
-
-      },function(error){
-        console.error(error);
-      });
-    }
-    else{
-      proceed_to_checkout(order_number);
-    }
-
-    //$state.go('checkout_address')
-  }
 })
