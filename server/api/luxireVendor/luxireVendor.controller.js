a85118f54e80dc49454faaa5f2818829cc4f4565
Mudassir <mudassir@azureiken.com>
2016-03-04 20:41:46 +0530

    Api for Luxire property & luxire vendor

---
+++ server/api/luxireVendor/luxireVendor.controller.js
@@ -0,0 +1,31 @@
+'use strict';
+/**
+ * Using Rails-like standard naming convention for endpoints.
+ * GET     /products              ->  index
+ * POST    /products              ->  create
+ * GET     /products/:id          ->  show
+ * PUT     /products/:id          ->  update
+ * DELETE  /products/:id          ->  destroy
+ */
+
+var _ = require('lodash');
+var http = require('request');
+var querystring = require('querystring');
+// var qs = require('qs');
+var env = require('../../config/constants');
+
+
+
+exports.index = function(req, res) {
+  console.log("luxire vendor  api is calling\n\n");
+
+  http
+    .get(env.spree.host+env.spree.luxireVendor, function(error, response, body){
+      if(response){
+        res.status(response.statusCode).send(body);
+      }
+      else{
+        res.status(500).send("Rails server not responding");
+      };
+  });
+};
