e9fb5d73f78d39d7ba1931b77c22c6428c9fac2d
Mudassir <mudassir@azureiken.com>
2016-04-17 00:41:45 +0530

    Taxonomy initial commit

---
+++ client/app/admin/taxonomy/taxonomy.controller.js
@@ -0,0 +1,105 @@
+angular.module('luxire')
+.controller('TaxonomyController', function($scope, TaxonomyService, $state,$rootScope,$timeout) {
+  $scope.taxonomyData = [];
+  /*Alerts to display messages*/
+  $rootScope.alerts = [];
+  var alert = function(){
+    this.type = '';
+    this.message = '';
+  };
+  $scope.close_alert = function(index){
+  $rootScope.alerts.splice(index, 1);
+  };
+
+//Display all the taxonomies
+  $scope.loading = true;
+  TaxonomyService.getTaxonomies().then(function(data) {
+    $scope.taxonomyData = data.data.taxonomies;
+    $scope.loading = false;
+  }, function(info) {
+      console.log(info);
+  })
+
+  //Hyperlinks in home which will invoke editpage
+    $scope.showEditTaxonomies = function(id) {
+      $state.go("admin.editTaxonomy", {id: id});
+    }
+
+    //create taxonomy function
+    $scope.createTaxonomy = function() {
+      if($scope.new_taxonomy_name == '' || $scope.new_taxonomy_name==undefined || $scope.new_taxonomy_name == 0){
+        document.getElementById("name").focus();
+        $scope.alerts.push({type: 'danger', message: 'Name should not be empty!'});
+      }else{
+          $scope.new_taxonomy = {
+            "name": $scope.new_taxonomy_name
+          };
+          $scope.taxonomyData.push($scope.new_taxonomy);
+          TaxonomyService.createTaxonomy($scope.new_taxonomy).then(function(data) {
+            $rootScope.alerts.push({type: 'success', message: 'Taxonomy created successfully!'});
+            $timeout(function(){
+              $state.go('admin.editTaxonomy', {id: data.data.id});
+            }, 3000)
+          }, function(error) {
+            console.log(error);
+            $rootScope.alerts.push({type: 'danger', message: 'Taxonomy Creation Failed!'}); // Changes made on 18th March
+          });
+      }
+  }
+
+    //Delete function
+    $scope.deleteTaxonomy = function(id, index) {
+      TaxonomyService.deleteTaxonomy(id).then(function(data) {
+        $scope.taxonomyData.splice(index, 1);
+        $rootScope.alerts.push({type: 'success', message: 'Taxonomy deleted successfully!'});
+      }, function(error) {
+        console.log(error);
+        $rootScope.alerts.push({type: 'danger', message: 'Taxonomy Deletion Failed!'});
+      })
+    }
+  })
+
+.controller('editTaxonomyController', function($scope, TaxonomyService, $stateParams, $state, $rootScope,$timeout) {
+  console.log("state params id : ", $stateParams.id);
+  $scope.taxons = [];
+
+  $rootScope.alerts = [];
+  var alert = function(){
+    this.type = '';
+    this.message = '';
+  };
+  $scope.close_alert = function(index){
+    $rootScope.alerts.splice(index, 1);
+  };
+
+  $scope.showEditCollections = function(id) {
+    console.log("Id of Taxon: " + id);
+    $state.go("admin.editCollections", {taxonomie_id:$stateParams.id, taxons_id: id});
+  }
+  $scope.loading = true ;
+  TaxonomyService.getTaxonomyById($stateParams.id).then(function(data) {
+    $scope.taxons = data.data.root.taxons;
+    $scope.taxonomyData = data.data.name;
+    $scope.loading = false;
+  }, function(info) {
+    console.log(info);
+  })
+
+
+  $scope.updateTaxonomy = function(id) {
+    var updatedTaxonomyObj = {
+      "name": $scope.taxonomyData
+    }
+
+
+    TaxonomyService.updateTaxonomy($stateParams.id, updatedTaxonomyObj).then(function(data) {
+      $rootScope.alerts.push({type: 'success', message: 'Taxonomy updated successfully!'});
+      $timeout(function(){
+        $state.go('admin.taxonomy');
+      }, 3000)
+    }, function(error) {
+      console.log(error);
+      $rootScope.alerts.push({type: 'danger', message: 'Taxonomy updation Failed!'});
+    })
+  }
+})
---
+++ client/app/admin/taxonomy/taxonomy.service.js
@@ -0,0 +1,33 @@
+angular.module('luxire')
+.service('TaxonomyService', function($http,$q){
+
+  this.getTaxonomies = function(){
+    return $http.get('/api/taxonomies');
+  }
+
+  this.createTaxonomy = function(taxonomie) {
+    console.log(taxonomie);
+    console.log('/api/taxonomies');
+    console.log(angular.toJson(taxonomie));
+    return $http.post('/api/taxonomies', angular.toJson(taxonomie));
+  }
+
+  this.getTaxonomyById= function(id){
+    console.log("id in getTaxonomyById service",id);
+    console.log('/api/taxonomies');
+    console.log('/api/taxonomies'+"/"+id);
+    return $http.get('/api/taxonomies'+"/"+id);
+  }
+
+
+  this.deleteTaxonomy= function(id){
+    console.log("id in service",id);
+    return $http.delete('/api/taxonomies'+"/"+id);
+  }
+  this.updateTaxonomy = function(id,taxonomie) {
+    console.log(id);
+    console.log("taxonomie",taxonomie);
+		 return $http.put('/api/taxonomies'+"/"+id, angular.toJson(taxonomie));
+	}
+
+})
