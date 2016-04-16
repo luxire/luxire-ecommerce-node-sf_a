9a24b61c58d8cd01e54e3548f3503e74633ef860
Mudassir <mudassir@azureiken.com>
2016-04-17 00:29:46 +0530

    Node api for properties

--- server/api/luxireProperties/index.js
+++ server/api/luxireProperties/index.js
@@ -5,7 +5,12 @@ var controller = require('./luxireProperties.controller');
 
 var router = express.Router();
 
-router.get('/', controller.index);
+router.get('/', controller.properties_Index);
+router.get('/:id', controller.properties_byId);
+router.post('/', controller.properties_create);
+router.put('/:id', controller.properties_update);
+router.delete('/:id', controller.properties_delete);
+
 
 
 module.exports = router;
--- server/api/luxireProperties/luxireProperties.controller.js
+++ server/api/luxireProperties/luxireProperties.controller.js
@@ -13,19 +13,89 @@ var http = require('request');
 var querystring = require('querystring');
 // var qs = require('qs');
 var env = require('../../config/constants');
+var path = require('path');
+//var constants = require(path.resolve('server/api/v1/version_constants'));
 
 
 
-exports.index = function(req, res) {
-  console.log("luxire properties api is calling\n\n");
 
+exports.properties_Index = function(req, res) {
+      console.log("properties index is calling..");
   http
-    .get(env.spree.host+env.spree.luxireProperties, function(error, response, body){
-      if(response){
-        res.status(response.statusCode).send(body);
+    .get(env.spree.host+env.spree.luxireProperties+"?token="+req.headers['X-Spree-Token'], function(error, response, body){
+      if(error){
+        res.status(500).send(error);
       }
       else{
-        res.status(500).send("Rails server not responding");
+        res.status(response.statusCode).send(body);
+      };
+    });
+
+};
+
+exports.properties_byId = function(req, res) {
+    console.log("properties by id is calling..");
+    console.log("url: ",env.spree.host+env.spree.luxireProperties+"/"+req.params.id);
+  http
+    .get(env.spree.host+env.spree.luxireProperties+"/"+req.params.id+"?token="+req.headers['X-Spree-Token'], function(error, response, body){
+      if(error){
+        res.status(500).send(error.syscall);
+      }
+      else{
+        res.status(response.statusCode).send(body);
+      };
+
+    });
+
+};
+
+exports.properties_create = function(req, res) {
+  console.log('style master create', req.body);
+  http.post({
+    uri: env.spree.host+env.spree.luxireProperties+'?token='+req.headers['X-Spree-Token'],
+    headers:{'content-type': 'application/json'},
+    body:JSON.stringify(req.body)
+  },function(error,response,body){
+    if(error){
+      res.status(500).send(error);
+    }
+    else{
+      res.status(response.statusCode).send(body);
+    };
+});
+
+};
+
+exports.properties_update = function (req, res){
+  console.log("update properties is calling..");
+  console.log("url: ",env.spree.host+env.spree.luxireProperties+'/'+req.params.id);
+        http
+          .put({
+            uri: env.spree.host+env.spree.luxireProperties+'/'+req.params.id+'?token='+req.headers['X-Spree-Token'],
+            headers:{'content-type': 'application/json'},
+            body:JSON.stringify(req.body)
+          }, function(error, response, body){
+            if(error){
+              res.status(500).send(error);
+            }
+            else{
+              res.status(response.statusCode).send(body);
+              console.log("Body",body);
+            };
+          });
+  };
+
+exports.properties_delete = function(req, res) {
+  console.log("delete properties is calling..");
+  console.log("url: ",env.spree.host+env.spree.luxireProperties+'/'+req.params.id);
+  http
+    .del(env.spree.host+env.spree.luxireProperties+"/"+req.params.id+"?token="+req.headers['X-Spree-Token'], function(error, response, body){
+      if(error){
+        res.status(500).send(error);
+      }
+      else{
+        res.status(response.statusCode).send(body);
       };
   });
+
 };
