2cda2464a8e76daa8974c4f877db5a1af6a04f03
Mudassir <mudassir@azureiken.com>
2016-04-18 17:53:15 +0530

    Added admin host api

--- client/app/admin/admin.services.js
+++ client/app/admin/admin.services.js
@@ -2,7 +2,7 @@ angular.module('luxire')
 .service('AdminConstants',function(){
 	this.api = {
 		host: 'http://104.215.254.150:3000/',
-		products: '',
+		products: '/api/v1/admin/products',
 		product_types: '/api/v1/admin/product_types',
 		measurement_types: '/api/v1/admin/measurement_types',//product attributes
 		style_masters: '/api/v1/admin/style_masters',
