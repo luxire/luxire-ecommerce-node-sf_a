2711ffac2f362c2a502e29d1505bce39297af8ee
Mudassir <mudassir@azureiken.com>
2016-06-29 17:13:26 +0530

    Directive to detect change

--- client/app/customer/customer.directives.js
+++ client/app/customer/customer.directives.js
@@ -31,3 +31,35 @@ angular.module('luxire')
       }
     };
 }])
+.directive("whenScrolled", function(){
+	return {
+		restrict:'A',
+		link: function(scope,elem,attrs){
+			raw = elem[0];
+			elem.bind("scroll", function(){
+					if(raw.scrollTop + raw.offsetHeight + 5 >= raw.scrollHeight){
+				//		scope.loading = true;
+						console.log('Firing next in dir');
+						scope.$apply(attrs.whenScrolled);
+					}
+			});
+		}
+	}
+})
+.directive("animateOnChange", function($timeout){
+  return function(scope, element, attr){
+    console.log('attr in directive', attr.animateOnChange);
+    scope.$watch(attr.animateOnChange, function(newVal, oldVal){
+      console.log('old val in dir', oldVal);
+      console.log('new val in dir', newVal);
+      if(newVal!==oldVal){
+        element.addClass('attr-changed');
+        $timeout(function(){
+          element.removeClass('attr-changed');
+        }, 1000)
+      }
+
+    })
+
+  };
+})
