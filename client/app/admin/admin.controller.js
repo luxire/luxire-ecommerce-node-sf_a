456e21b3b1497b0c22f15a724667dbeafd3c2357
Mudassir <mudassir@azureiken.com>
2016-08-13 12:55:24 +0530

    Added functionlity to link admin app to crm

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -17,6 +17,10 @@
    $scope.user_popover = {
     templateUrl: 'user_popover.html',
   };
+  
+  $scope.go_to_crm = function(){
+    window.open("http://crm.cloudhop.in/login?data=admin@example.com&data2=admin", "_blank");
+  }
 
    $scope.mouseoverNav = function(){
      $scope.showDefaultNav = true;
--- client/app/admin/default/default.controller.js
+++ client/app/admin/default/default.controller.js
@@ -1,4 +1,7 @@
 angular.module('luxire')
 .controller('DefaultController',function($scope, $stateParams){
   console.log($stateParams);
+  $scope.go_to_crm = function(){
+    window.open("http://crm.cloudhop.in/login?data=admin@example.com&data2=admin", "_blank");
+  }
 });
