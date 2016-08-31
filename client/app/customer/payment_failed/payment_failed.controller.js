4cf7a34750ece78e0d97bb7c7796077e096e4bbc
Mudassir <mudassir@azureiken.com>
2016-08-31 14:30:48 +0530

    Added View for payment failed

---
+++ client/app/customer/payment_failed/payment_failed.controller.js
@@ -0,0 +1,4 @@
+angular.module('luxire')
+.controller('PaymentFailedController', ['$scope', '$rootScope', '$state', 'ImageHandler', 'CustomerProducts', '$uibModal', 'CustomerOrders', 'CustomerUtils', function($scope, $rootScope, $state, ImageHandler, CustomerProducts, $uibModal, CustomerOrders, CustomerUtils){
+  console.log('PaymentFailedController');
+}])
