61764114ffd1a81e26620e8ff83dcb9eb908851e
Mudassir <mudassir@azureiken.com>
2016-10-07 14:53:08 +0530

    Modified countries index api to return requested country from ip

--- server/api/country/country.controller.js
+++ server/api/country/country.controller.js
@@ -6,20 +6,49 @@ var env = require('../../config/constants');
 
 
 exports.index = function(req, res){
+  var req_ip = "";
+  var req_cur = "";
+  var forwarded_for = "";
+  forwarded_for = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  console.log('req ip',req.ip,'forwarded for ip', forwarded_for);
+
+  req_ip = forwarded_for === "127.0.0.1" ? '' : forwarded_for;
+
+  function countries(country){
+    var country = country ? country : {};
+    http
+      .get({
+      uri: env.spree.host+env.spree.countries+'/all.json',
+      headers:{'content-type': 'application/json'},
+      body:''
+    },function(error,response,body){
+      if(error == null){
+        res.status(response.statusCode).send({requested_country: country, countries: body});
+      }
+      else{
+        res.status(500).send("Rails Server Not Responding");
+      }
+
+      });
+  }
+
   http
     .get({
-    uri: env.spree.host+env.spree.countries+'/all.json',
-    headers:{'content-type': 'application/json'},
-    body:''
-  },function(error,response,body){
-    if(error == null){
-      res.status(response.statusCode).send(body);
-    }
-    else{
-      res.status(500).send("Rails Server Not Responding");
-    }
+      uri: 'http://freegeoip.net/json/'+req_ip
+    }, function(error, response, body){
+        if(error){
+          console.log('error getting location', error.syscall);
+          countries();
+        }
+        else{
+          countries(body);
+        };
 
-    });
+
+
+  });
+
+
   };
 
   exports.search = function(req, res){
