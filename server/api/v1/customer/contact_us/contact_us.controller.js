507946e58f80d07d4540f3bd0f8ffb756541aa7d
Mudassir <mudassir@azureiken.com>
2016-12-01 03:20:21 +0530

    Integration with backend service

--- server/api/v1/customer/contact_us/contact_us.controller.js
+++ server/api/v1/customer/contact_us/contact_us.controller.js
@@ -6,6 +6,22 @@ var path = require('path');
 var constants = require(path.resolve('server/api/v1/version_constants'));
 
 exports.contact_us = function(req, res){
-  console.log('req.body', req.body);
-  res.status(200).send('');
+  console.log('req.body from user', req.body);
+  http
+    .post({
+      uri: constants.spree.host+constants.spree.contact_us,
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(req.body)
+    }, function(error, response, body){
+      if(error){
+        res.status(500).send(error.syscall);
+      }
+      else{
+        console.log('response from contact us', body);
+        res.status(response.statusCode).send(body);
+      };
+
+  });
 };
