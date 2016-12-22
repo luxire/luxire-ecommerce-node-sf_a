e17645da149be2fa8d2184e321d96e67356847b2
Mudassir <mudassir@azureiken.com>
2016-12-22 19:27:27 +0530

    Removed unnecessary logs

--- client/app/customer/customer_home/customer_home.controllers.js
+++ client/app/customer/customer_home/customer_home.controllers.js
@@ -1,6 +1,5 @@
 angular.module('luxire')
 .controller('CustomerHomeController', function($scope, $state,CustomerConstants, $window, CustomerProducts, ImageHandler, $rootScope, $location, CustomerOrders, $timeout, $sce){
-
   $scope.sce = $sce;
   $scope.loading = true;
   var default_collection = {
@@ -10,39 +9,25 @@ angular.module('luxire')
     taxon_id: CustomerConstants.default.taxon_id,
   };
   $scope.product_taxonomies = [];
-
-
   $timeout(function(){
     $('.carousel').carousel();
   }, 0);
   $scope.prev_product_slide = function(index){
-    console.log('index', index);
     $('.carousel').carousel('prev');
   };
   $scope.next_product_slide = function(index){
-    console.log('index', index);
-
     $('.carousel').carousel('next');
   };
 
   CustomerProducts.taxonomy_index()
   .then(function(data){
     $scope.product_taxonomies = data.data.taxonomies;
-    console.log('taxonomies', data.data.taxonomies);
   }, function(error){
     console.log('error', error);
   });
   $scope.go_to_product_listing = function(taxonomy_name, taxon_name){
     $state.go('customer.product_listing', default_collection);
   };
-  // CustomerOrders.get_order_by_cookie()
-  // .then(function(data){
-  //   console.log('fetched order', data.data);
-  //   $rootScope.luxire_cart = data.data;
-  // },
-  // function(error){
-  //   console.error(error);
-  // });
   function capitalizeFirstLetter(string) {
       return string.charAt(0).toUpperCase() + string.slice(1);
   }
@@ -60,22 +45,6 @@ angular.module('luxire')
   };
 
   $scope.product_type_carousels = [
-    // {
-    //   id: 2,
-    //   image: 'assets/images/customer/luxire-banner/jacket.jpg',
-    //   link: '',
-    //   text: 'Bespoke Suit Jackets',
-    //   permalink: 'jackets'
-    // }
-    // ,
-    // {
-    //   id: 1,
-    //   image: 'assets/images/customer/luxire-banner/shirt.jpg',
-    //   link: '',
-    //   text: 'Hand-Stitched Luxury Shirts',
-    //   permalink: 'shirts'
-    //
-    // }
     {
       id: 1,
       image: 'assets/images/customer/luxire-banner/slide-01.jpg',
@@ -135,23 +104,13 @@ angular.module('luxire')
   }
 
   $scope.get_display_width = function(){
-    // console.log('area', $("#activeReviewArea").innerWidth());
-    // console.log('img', $("#activeReviewImage").innerWidth());
-    // $timeout(function(){
-    // },0)
   };
 
   $scope.change_active_click_bait = function(id){
-    console.log('mouse over', id);
     $scope.active_click_bait.id=id;
     if($scope.active_click_bait_detail.id!==id){
-      // $scope.active_click_bait_detail.id = -1;
       $scope.active_click_bait_detail.id = id;
-      // $timeout(function () {
-      //
-      // }, 500);
     }
-
   };
 
   $scope.click_baits = [
@@ -218,75 +177,6 @@ angular.module('luxire')
 
 
   ]
-
-
-  // $scope.click_baits = [
-  //   {
-  //     id: 1,
-  //     y_pos: '25%',
-  //     x_pos: '48.5%',
-  //     name: 'Suit Style',
-  //     text_line: 'This is a sample text animation for Suit Style'
-  //   },
-  //   {
-  //     id: 2,
-  //     y_pos: '28%',
-  //     x_pos: '48.5%',
-  //     name: 'Monogram',
-  //     text_line: 'This is a sample text animation for Monogram'
-  //   },
-  //   {
-  //     id: 3,
-  //     y_pos: '30.5%',
-  //     x_pos: '49.5%',
-  //     name: 'Pocket Squares',
-  //     text_line: 'This is a sample text animation for Pocket Squares'
-  //   },
-  //   {
-  //     id: 4,
-  //     y_pos: '37.5%',
-  //     x_pos: '49.5%',
-  //     name: 'Fit & Size',
-  //     text_line: 'This is a sample text animation for Fit & Size'
-  //   },
-  //   {
-  //     id: 5,
-  //     y_pos: '44%',
-  //     x_pos: '54.5%',
-  //     name: 'Shirt Cuff Style',
-  //     text_line: 'This is a sample text animation for Shirt Cuff Style'
-  //   },
-  //   {
-  //     id: 6,
-  //     y_pos: '77%',
-  //     x_pos: '53.5%',
-  //     name: 'Shoes',
-  //     text_line: 'This is a sample text animation for Shoes'
-  //   },
-  //   {
-  //     id: 7,
-  //     y_pos: '56%',
-  //     x_pos: '51%',
-  //     name: 'Trousser Fit',
-  //     text_line: 'This is a sample text animation for Trousser Fit'
-  //   },
-  //   {
-  //     id: 8,
-  //     y_pos: '40%',
-  //     x_pos: '45.5%',
-  //     name: 'Button Colour',
-  //     text_line: 'This is a sample text animation for Button Colour'
-  //   },
-  //   {
-  //     id: 9,
-  //     y_pos: '35%',
-  //     x_pos: '44.5%',
-  //     name: 'Shirt Placket',
-  //     text_line: 'This is a sample text animation for Shirt Placket'
-  //   }
-  //
-  // ]
-
   $scope.loading = false;
 
 
@@ -373,14 +263,10 @@ angular.module('luxire')
   };
   $(window).resize(function(){
     $scope.get_review_width();
-    console.log('window resized');
   });
 
   $scope.change_active_review_index = function(index){
-    console.log('current active index', $scope.active_customer_review_index);
-    console.log('index', index);
     $scope.active_customer_review_index = index;
-    console.log('new active index', $scope.active_customer_review_index);
   };
 
   $scope.getImage = function(url){
@@ -413,13 +299,10 @@ angular.module('luxire')
   ];
 
   $scope.beginLineAnimation = function(){
-    console.log('animate line', document.getElementById('animateLine'));
     document.getElementById('animateLine').beginElement()
-
   };
 
   $scope.go_to_listing = function(taxonomy){
-    console.log('selected_taxonomy', taxonomy);
     if(taxonomy.root.taxons && taxonomy.root.taxons.length>0){
       var listing = {
         taxonomy_name: taxonomy.name,
