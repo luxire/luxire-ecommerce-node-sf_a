341ac3beb2368f5628e839cd9e1bc70dea72e1bc
Mudassir <mudassir@azureiken.com>
2017-01-06 14:58:39 +0530

    Removed log statements

--- client/app/admin/productTypes/productType.services.js
+++ client/app/admin/productTypes/productType.services.js
@@ -1,12 +1,9 @@
 angular.module('luxire')
 .service('productTypeService', function($http,$q, AdminConstants){
-  //Get all product Attributes
-
+  //Get all product types
   this.getProductTypes = function(){
-    console.log('get all products', AdminConstants.api.product_types);
 		return $http.get(AdminConstants.api.product_types);
 	}
-
 
   this.deleteProductType = function(id) {
     return $http.delete(AdminConstants.api.product_types+"/"+id);
@@ -17,34 +14,25 @@ angular.module('luxire')
 		return $http.post(AdminConstants.api.product_types, angular.toJson(productType));
 	}
 
-
   this.updateProductType = function(id,productType) {
-    console.log("productType",productType);
 		 return $http.put(AdminConstants.api.product_types+"/"+id, angular.toJson(productType));
 	}
 
   this.getMeasurementTypes = function(){
   	return $http.get(AdminConstants.api.measurement_types);
   }
+
   this.getProductTypeById= function(id){
     return $http.get(AdminConstants.api.product_types+"/"+id);
   }
   /*upload image*/
-
   this.update_image = function(image, id){
-    console.log("update image service");
-    console.log("image value passed",image);
-    console.log("id passed",id);
-  //  console.log('image', image);
    var fd = new FormData();
    fd.append('image', image);
-   console.log("value appended");
    return $http.put(AdminConstants.api.product_types+'/'+id+'/images', fd, {
        transformRequest: angular.identity,
        headers: {'Content-Type': undefined}
     });
-    console.log("end of update service");
- };
-
+  };
  /*-------*/
 })
