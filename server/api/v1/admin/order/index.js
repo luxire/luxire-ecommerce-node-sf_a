e77ae4cce86dc31622e105d0cbb5cced2f7918b9
manish@azureiken.com <luxire@cloudhop-store-lux-stage.mrmphw5fh4outbzbdejx53rcdd.ix.internal.cloudapp.net>
2017-05-18 06:03:08 +0000

    Added routes for cancel, refund and ship order

--- server/api/v1/admin/order/index.js
+++ server/api/v1/admin/order/index.js
@@ -8,5 +8,8 @@ router.get('/',controller.index);
 router.put('/update_status', controller.update_status);
 router.put('/update_line_item_status', controller.update_line_item_status);
 router.get('/:number', controller.show);
+router.put('/cancel/:id',controller.cancel);
+router.put('/refund', controller.refund);
+router.put('/ship/:id', controller.ship);
 
 module.exports = router;
