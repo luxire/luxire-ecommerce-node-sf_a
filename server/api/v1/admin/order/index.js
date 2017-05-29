d34f7e711ca3b1740214b0d374053a5f8db740e7
Mudassir <mudassir@azureiken.com>
2017-05-29 14:50:13 +0530

    Added new route for fetching recently completed order

--- server/api/v1/admin/order/index.js
+++ server/api/v1/admin/order/index.js
@@ -4,6 +4,7 @@ var express =  require('express');
 var controller = require('./order.controller');
 var router = express.Router();
 
+router.get('/getRecentlyCompletedOrder', controller.getRecentlyCompletedOrder);
 router.get('/',controller.index);
 router.put('/update_status', controller.update_status);
 router.put('/update_line_item_status', controller.update_line_item_status);
@@ -11,5 +12,6 @@ router.get('/:number', controller.show);
 router.put('/cancel/:id',controller.cancel);
 router.put('/refund', controller.refund);
 router.put('/ship/:id', controller.ship);
+
 
 module.exports = router;
