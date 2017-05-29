50e25df0dc16b6a59a231cd0606251a9a999a427
Mudassir <mudassir@azureiken.com>
2017-05-29 14:52:07 +0530

    Added routing for sorting collections

--- server/api/v1/admin/taxonomies/index.js
+++ server/api/v1/admin/taxonomies/index.js
@@ -8,5 +8,7 @@ var router = express.Router();
 router.post('/:id/taxons', controller.createTaxonsManually);
 router.put('/:id/taxons/:taxonsId', controller.updateTaxons);
 router.post('/createRuleBasedCollection', controller.createDynamicCollection);
+router.put('/changePosition', controller.changePosition);
+router.put('/sort', controller.sortCollection);
 
 module.exports = router;
