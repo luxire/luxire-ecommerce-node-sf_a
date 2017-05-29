a9ed7334ef8b5e5866d4eb37b358315d6572ead4
Kamalakannan Krishnaraj <kamal@Tanays-MacMiniMavericks.local>
2017-05-29 19:18:41 +0530

    bug fixes

--- client/app/admin/product/editModalInstance.controller.js
+++ client/app/admin/product/editModalInstance.controller.js
@@ -1,8 +1,8 @@
 angular.module('luxire')
 
-.controller('editModalInstanceCtrl',function($scope,luxireProperties, $uibModalInstance, luxireStock, createProductModalService){
+.controller('editModalInstanceCtrl',function($scope,products,luxireProperties, $uibModalInstance, luxireStock, createProductModalService){
   $scope.modalData = luxireStock;
-
+  console.log('the modalData:',$scope.modalData);
   // start: inventory measuring unit
   luxireProperties.luxirePropertiesIndex().then(function(data) {
     $scope.luxireProperties = data.data;
@@ -10,12 +10,17 @@ angular.module('luxire')
   }, function(info){
     console.log(info);
   })
-
+  $scope.updateStockId = $scope.modalData.product || $scope.modalData.id;
   // end: inventory measuring unit
 
  $scope.luxireStock=$scope.modalData.data; // binding of inventory data to the inventory modal
 
     $scope.ok = function () { // ok button to pass values from inventory controller to main controller
+      // $scope.loading = true;
+      products.updateStock($scope.updateStockId,$scope.luxireStock).then(function(data){
+        // $scope.
+        $scope.alerts.push({type:'success',message:'Inventory updated successfully!'});
+      })
       $uibModalInstance.close($scope.luxireStock);
     };
 
